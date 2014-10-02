d7e108826c1478382c92f85144410d7ec58c670c
Andrew Dunham <andrew@du.nham.ca>
2014-10-01 18:30:29 -0700

    Add OpenVPN protocol

---
+++ protocol_openvpn.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"encoding/binary"
+)
+
+type OpenVpnProtocol struct {
+}
+
+func init() {
+	RegisterProtocol(OpenVpnProtocol{})
+}
+
+func (p OpenVpnProtocol) Detect(buf []byte) (bool, error) {
+	if len(buf) < 2 {
+		return false, ErrMoreData
+	}
+
+	// TODO: this is rather fragile - it relies on us receiving the entirety of
+	// the first packet immediately
+	plen := int(binary.BigEndian.Uint16(buf[0:2]))
+	return plen == len(buf) - 2, nil
+}
+
+func (p OpenVpnProtocol) Name() string {
+	return "openvpn"
+}
